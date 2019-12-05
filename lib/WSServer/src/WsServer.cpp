f4d8222f9a1d2806a358618b8c3c6f6be99ba618
gustavosinbandera1 <gustavosinbandera1@hotmail.com>
2019-12-05 17:38:09 -0500

    save state

--- lib/WSServer/src/WsServer.cpp
+++ lib/WSServer/src/WsServer.cpp
@@ -3,15 +3,26 @@
 #include <string.h>
 
 
+#include "freertos/FreeRTOS.h"
+#include "freertos/queue.h"
+#include "freertos/task.h"
+
+
+#include "esp_system.h" // for esp_random
+#include "mbedtls/base64.h"
+#include "mbedtls/sha1.h"
+#include <string.h>
+
+
 namespace server {
 
 QueueHandle_t client_queue;
 QueueHandle_t persisten_queue;
-static QueueHandle_t xwebsocket_queue;                    // to hold the clients that send messages
-static TaskHandle_t xtask;                        // the task itself
-static SemaphoreHandle_t xwebsocket_mutex;        // to lock the client array
-static int clients[WEBSOCKET_SERVER_MAX_CLIENTS]; // holds list of clients
-const int DELAY = 1000 / portTICK_PERIOD_MS;      // 1 second
+static QueueHandle_t websocket_queue;                       // to hold the clients that send messages
+static TaskHandle_t xtask;                                  // the task itself
+static SemaphoreHandle_t websocket_mutex;                   // to lock the client array
+static socket_client clients[WEBSOCKET_SERVER_MAX_CLIENTS]; // holds list of clients
+const int DELAY = 1000 / portTICK_PERIOD_MS;                // 1 second
 
 WsServer::WsServer()
 {
@@ -25,23 +36,107 @@ WsServer::~WsServer()
     xQueueReset(client_queue);
 }
 
-
-static void background_callback(int conn, enum netconn_evt evt, u16_t len)
+// handles websocket events
+void wsCallback(int conn, wsEvent event)
 {
-    switch (evt) {
-    case NETCONN_EVT_RCVPLUS:
-        xQueueSendToBack(xwebsocket_queue, &conn, WEBSOCKET_SERVER_QUEUE_TIMEOUT);
-        break;
-    default:
-        break;
+    ESP_LOGI("USING wsCALLBACK ", "THIS IS THE SOCKET WAS ADDED TO THE CLIENTS ARRAY------%d:)", conn);
+    ESP_LOGI("USING wsCALLBACK ", "THIS IS THE SOCKET type %d", event);
+    //we need to check for disconnected sockets
+}
+
+char* strnstr(const char* s, const char* find, size_t slen)
+{
+    char c, sc;
+    size_t len;
+
+    if ((c = *find++) != '\0') {
+        len = strlen(find);
+        do {
+            do {
+                if (slen-- < 1 || (sc = *s++) == '\0')
+                    return (NULL);
+            } while (sc != c);
+            if (len > slen)
+                return (NULL);
+        } while (strncmp(s, find, len) != 0);
+        s--;
+    }
+    return ((char*)s);
+}
+
+void WsServer::addClient(int conn, callBkAddClient callck)
+{
+    int i = 0;
+    char buff[255];
+    int n;
+    ESP_LOGI("ENTRAMOS---->", "EN EL server %d", conn);
+    read(conn, buff, sizeof(buff));
+    printf("From client: %s\t To client : ", buff);
+
+    if (strnstr(buff, WS_HEADER, MAX)) {
+        unsigned char encoded_key[32];
+        char key[64];
+        char* key_start = strnstr(buff, WS_KEY, MAX);
+        if (key_start) {
+            key_start += 19;
+            char* key_end = strnstr(key_start, "\r\n", MAX);
+            if (key_end) {
+                int len = sizeof(char) * (key_end - key_start);
+                if (len + sizeof(WS_GUID) < sizeof(key) && len > 0) {
+                    // Concatenate key
+                    memcpy(key, key_start, len);
+                    strlcpy(&key[len], WS_GUID, sizeof(key));
+                    printf("Resulting key: %s\n", key);
+                    // Get SHA1
+                    unsigned char sha1sum[20];
+                    mbedtls_sha1((unsigned char*)key, sizeof(WS_GUID) + len - 1, sha1sum);
+                    //Base64 encode
+                    unsigned int olen;
+                    mbedtls_base64_encode(NULL, 0, &olen, sha1sum, 20); //get length
+                    int ok = mbedtls_base64_encode(encoded_key, sizeof(encoded_key), &olen, sha1sum, 20);
+                    if (ok == 0) {
+                        // hs->is_websocket = 1;
+                        encoded_key[olen] = '\0';
+                        printf("Base64 encoded: %s\n", encoded_key);
+                        // Send response
+                        char buf[256];
+                        uint16_t len = snprintf(buf, sizeof(buf), WS_RSP, encoded_key);
+                        //http_write(pcb, buf, &len, TCP_WRITE_FLAG_COPY);
+                        //return ERR_OK;
+                        write(conn,buf,sizeof(buf));
+                    }
+                } else {
+                    printf("Key overflow\n");
+                    //return ERR_MEM;
+                }
+            }
+        } else {
+            printf("Malformed packet\n");
+            //return ERR_ARG;
+        }
+        /* if (strncmp(buff, "GET", 4)) {
+        char uri[16];
+        const int max_uri_len = 16;
+        char *sp1, *sp2;
+        // extract URI 
+        sp1 = buff + 4;
+        sp2 = (char*)memchr(sp1, ' ', max_uri_len);
+        int len = sp2 - sp1;
+        memcpy(uri, sp1, len);
+        uri[len] = '\0';
+        printf("uri: %s\n", uri);
+    } */
     }
 }
+
 
 //task server should have to run forever
 void WsServer::runServer()
 {
     // socket create and verification
     sockfd_ = socket(AF_INET, SOCK_STREAM, 0);
+    websocket_mutex = xSemaphoreCreateMutex();
+
     if (sockfd_ < 0) {
         ESP_LOGI("*****************", "was not possible to create the socket\n");
         exit(0);
@@ -65,13 +160,20 @@ void WsServer::runServer()
         exit(0);
     }
     len_ = sizeof(cli_addr_);
+    connfd_ = new int();
 
+    ESP_LOGI("after loop", "task starting---handle clients");
+    for (int i = 0; i < WEBSOCKET_SERVER_MAX_CLIENTS; i++) {
+        clients[i].conn = 0;
+    }
     //Waitting for new connections forever
     do {
-        connfd_ = new int();
         *connfd_ = accept(sockfd_, (SA*)&cli_addr_, &len_);
         if (*connfd_ > 0) {
             ESP_LOGI("SERVER_LOG --63", "server acccept the client...\n");
+            xSemaphoreTake(websocket_mutex, portMAX_DELAY);
+            addClient(*connfd_, wsCallback); //add client to array clients connected
+            xSemaphoreGive(websocket_mutex);
             xQueueSendToBack(client_queue, &connfd_, portMAX_DELAY); //this queue contain all connected clients
         } else {
             ESP_LOGI("SERVER_LOG--66", "server acccept failed...\n");
@@ -91,21 +193,35 @@ void WsServer::runServer()
  * 
  */
 
+void WsServer::readMessage(int i)
+{
+    ESP_LOGI("LEYENDO----->", "socket %d", i);
+    struct netconn* conn;
+}
+
 
 void WsServer::handleClients()
 {
     const static char* TAG = "handleClients";
     int* conn;
+    char buff[200];
+
+    int n;
+    static err_t err;
+
     ESP_LOGI(TAG, "task starting---handle clients");
+    for (int i = 0; i < WEBSOCKET_SERVER_MAX_CLIENTS; i++) {
+        clients[i].conn = 0;
+    }
     for (;;) {
         xQueueReceive(client_queue, &conn, portMAX_DELAY);
-        ESP_LOGI(TAG, "RECIBIENDO DATA DEL SOCKET------------------->>>>>>>");
-        if (conn) {
-            ESP_LOGI("tcpserver-->>90", "en la cola conn != de cero");
-          //  addClient();
-            xQueueSendToBack(persisten_queue, &conn, portMAX_DELAY); //this queue contain all future websocket clients
+        if (*conn != 0) {
+            xSemaphoreTake(websocket_mutex, portMAX_DELAY);
+            //here we have array of clients to send a receive messages and the actual client in queue
+            ESP_LOGI("ENTRAMOS---->", "RECIBIMOS EN LA QUEUE %d", *conn);
+
+            xSemaphoreGive(websocket_mutex);
         }
-        delete conn;
     }
     vTaskDelete(NULL);
 }
--- lib/WSServer/src/WsServer.h
+++ lib/WSServer/src/WsServer.h
@@ -7,7 +7,7 @@
 #include <cstdint>
 
 
-
+#include "sdkconfig.h"
 #include <netdb.h>
 #include <netinet/in.h>
 #include <stdio.h>
@@ -15,21 +15,46 @@
 #include <string.h>
 #include <sys/socket.h>
 #include <sys/types.h>
-#include "sdkconfig.h"
 
 #define MAX 255
-#define PORT 80
+#define PORT 8080
 #define SA struct sockaddr
 
 #define WEBSOCKET_SERVER_MAX_CLIENTS CONFIG_WEBSOCKET_SERVER_MAX_CLIENTS
 #define WEBSOCKET_SERVER_QUEUE_TIMEOUT CONFIG_WEBSOCKET_SERVER_QUEUE_TIMEOUT
 
-
 namespace server {
+
+
+const char WS_HEADER[] = "Upgrade: websocket\r\n";
+const char WS_GUID[] = "258EAFA5-E914-47DA-95CA-C5AB0DC85B11";
+const char WS_KEY[] = "Sec-WebSocket-Key: ";
+const char WS_RSP[] = "HTTP/1.1 101 Switching Protocols\r\n" \
+                      "Upgrade: websocket\r\n" \
+                      "Connection: Upgrade\r\n" \
+                      "Sec-WebSocket-Accept: %s\r\n\r\n";
 
 extern QueueHandle_t client_queue;
 extern QueueHandle_t persisten_queue;
 
+typedef struct {
+    int conn;
+    bool isConnected = false;
+    void (*clienteCallback)(int conn);
+    void (*serverCallback)(uint8_t index, char* msg, uint64_t len);
+} socket_client;
+
+typedef enum {
+    CONNECTED,
+    DISCONNECT,
+    TEXT,
+    BIN,
+    PING,
+    PONG
+} wsEvent;
+
+
+typedef void (*callBkAddClient)(int conn, wsEvent event);
 
 class WsServer
 {
@@ -39,16 +64,19 @@ public:
     //http server start
     void start(void);
     static const int socket_queue;
-private:
 
+private:
     int sockfd_, *connfd_;
     socklen_t len_;
     struct sockaddr_in serveraddr_, cli_addr_;
     static const int client_queue_size = 5;
-    
+
     void runServer(void);
     void handleClients(void);
-    void addClient();
+    //void addClient(int conn);
+    void addClient(int conn, callBkAddClient);
+
+    void readMessage(int i);
     //wrapper to use member functions inside callbacks for tasks
     inline static void callbackServerTask(void* context) { static_cast<WsServer*>(context)->runServer(); }
     //wrapper // to execute handleClients
