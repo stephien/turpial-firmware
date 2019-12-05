7fb3a3230126dab34391dab2143bda87161f2c31
gustavosinbandera1 <gustavosinbandera1@hotmail.com>
2019-12-04 20:52:57 -0500

    remove code from main

--- src/main.cpp
+++ src/main.cpp
@@ -25,24 +25,8 @@
 
 #include "defaults.h"
 #include "TcpServer.h"
-//#include "WebSocketServer.h"
-
-
-/* #include <netdb.h>
-#include <netinet/in.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <sys/socket.h>
-#include <sys/types.h>
-#define MAX 255
-#define PORT 80
-#define SA struct sockaddr */
-
 
 static const char* TAG = "app_main";
-//WebSocket frame receive queue
-//QueueHandle_t WebSocket_rx_queue;
 
 esp_err_t getIsConfigured(bool& is_configured)
 {
@@ -78,34 +62,6 @@ esp_err_t getIsConfigured(bool& is_configured)
 }
 
 
-// Function designed for chat between client and server.
-void func(int sockfd)
-{
-    char buff[MAX];
-    int n;
-    for (;;) {
-        bzero(buff, MAX);
-        // read the message from client and copy it in buffer
-        read(sockfd, buff, sizeof(buff));
-        // print buffer which contains the client contents
-        printf("From client: %s\t To client : ", buff);
-        bzero(buff, MAX);
-        n = 0;
-        while (n < MAX) {
-            buff[n] = getchar();
-            if (buff[n] == '\n') break;
-            n += 1;
-        }
-        // and send that buffer to client
-        write(sockfd, buff, sizeof(buff));
-
-        // if msg contains "Exit" then server exit and chat ended.
-        if (strncmp("exit", buff, 4) == 0) {
-            printf("Server Exit...\n");
-            break;
-        }
-    }
-}
 
 
 extern "C" void app_main()
@@ -180,52 +136,3 @@ extern "C" void app_main()
     // server = new server::WebSocketServer();
     // server->start();
     // server->ws_server_start();
-
-//server here /////////////////////////////////
-   /*  int sockfd, connfd;
-    socklen_t len;
-    struct sockaddr_in servaddr, cli;
-
-    // socket create and verification
-    sockfd = socket(AF_INET, SOCK_STREAM, 0);
-    if (sockfd == -1) {
-        ESP_LOGI("*****************", "socket creation failed...\n");
-        exit(0);
-    } else
-        ESP_LOGI("*****************", "Socket successfully created..\n");
-    bzero(&servaddr, sizeof(servaddr));
-
-    // assign IP, PORT
-    servaddr.sin_family = AF_INET;
-    servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
-    servaddr.sin_port = htons(PORT);
-
-    // Binding newly created socket to given IP and verification
-    if ((bind(sockfd, (SA*)&servaddr, sizeof(servaddr))) != 0) {
-        ESP_LOGI("******************", "socket bind failed...\n");
-        exit(0);
-    } else
-        ESP_LOGI("**************************", "Socket successfully binded..\n");
-
-    // Now server is ready to listen and verification
-    if ((listen(sockfd, 5)) != 0) {
-        ESP_LOGI("*****************************", "Listen failed...\n");
-        exit(0);
-    } else
-        ESP_LOGI("*****************************", "Server listening..\n");
-    len = sizeof(cli);
-
-
-    // Accept the data packet from client and verification
-    connfd = accept(sockfd, (SA*)&cli, &len);
-    if (connfd < 0) {
-        printf("server acccept failed...\n");
-        exit(0);
-    } else {
-        printf("server acccept the client...\n");
-     ESP_LOGI("VALUE OF SOCKET FILE DESCRIPTOR CONECTION:   ", "Value: %d", connfd);
-    }
-    // Function for chatting between client and server
-
-    func(connfd);
-} */
